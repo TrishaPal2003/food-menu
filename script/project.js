970a95a0d69833fe12103203acf2f87413f0c3c3
TrishaPal2003 <trishapal160@gmail.com>
2025-01-31 22:30:54 +0600

    dont know

--- script/project.js
+++ script/project.js
@@ -6,20 +6,26 @@ const recipecontainer = document.querySelector('.reciepe-container')
 const fetchreceipes =async(query) => {
     const data =await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`)
     const response =await data.json();
+
+    recipecontainer.innerHTML="";
     
     response.meals.forEach(meal => {
        const reciepeDiv =document.createElement('div');
        reciepeDiv.classList.add('recipe');
        reciepeDiv.innerHTML=`
-        <div class="image-container"style="background-color:white">
+        <div class="image-container">
           <img src="${meal.strMealThumb}">
-          <h1>${meal.strMeal}</h1>
+          <h3>${meal.strMeal}</h3>
+          <p>${meal.strArea} Dish</p>
+          <p>${meal.strCategory}</p>
           <button class="button-container">View Recipe</button>
           </div>
        
        `
        
        recipecontainer.appendChild(reciepeDiv);
+
+       
 
     });
     document.querySelectorAll(".button-container").forEach(button => {
